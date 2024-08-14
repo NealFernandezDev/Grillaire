288b90a8cd0b9013c4f4154eedd409e270e9bcde
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-08-13 20:58:01 -0500

    lower header, logo, and navigation styled

--- src/Components/Assets/Header/Header.jsx
+++ src/Components/Assets/Header/Header.jsx
@@ -1,39 +1,40 @@
 import React from 'react'
 import Logo from '../images/logo.png'
 import './Header.scss'
-import Instagram from '../images/instagram (1).png'
+import Instagram from '../images/instagram.png'
 import Facebook from '../images/facebook.png'
 import Twitter from '../images/twitter.png'
 import Youtube from '../images/youtube.png'
+import Location from '../images/map.png'
+import Phone from '../images/phone-call.png'
 
 const Header = () => {
   return (
     <div id='header'>
       <div id="upper-header">
         <div id="phone-address">
-          <p>2900 Lapeer Rd, Port Hurons, MI 49070 +1 
+          <p><img id='phone' src={Location} alt="Location"/>2900 Lapeer Rd, Port Hurons, MI 49070 <img id="phone" src={Phone} alt="Phone"/>+1 
             (800) 478-42-51</p>
         </div>
         <div id ="social-icons">
-          <div id="facebook-bg"></div>
-          <img src={Facebook} alt="Facebook"/>
-          <img src={Twitter} alt="Twitter"/>
-          <img src={Instagram} alt="Instagram"/>
-          <img src={Youtube} alt="Youtube"/>
+          <a href="/#"><img id="social1" src={Facebook} alt="Facebook"/></a>
+          <a href="/#"><img id='social1' src={Twitter} alt="Twitter"/></a>
+          <a href="/#"><img id='social' src={Instagram} alt="Instagram"/></a>
+          <a href="/#"><img id='social' src={Youtube} alt="Youtube"/></a>
         </div>
       </div>
       <div id="lower-header">
         <div>
           <img src={Logo} alt='Logo' />
         </div>
-        <div>
+        <div id="navigation">
           <nav>
             <ul>
-              <li>HOME</li>
-              <li>ABOUT US</li>
-              <li>MENU</li>
-              <li>GALLERY</li>
-              <li>CONTACTS</li>
+              <li><a href="/#">HOME</a></li>
+              <li><a href="/#">ABOUT US</a></li>
+              <li><a href="/#">MENU</a></li>
+              <li><a href="/#">GALLERY</a></li>
+              <li><a href="/#">CONTACTS</a></li>
             </ul>
           </nav>
         </div>
